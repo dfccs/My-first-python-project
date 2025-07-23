64b81f965158d609f1fb187271ce84f974a3fb21
dfccs <125632971+dfccs@users.noreply.github.com>
2025-07-22 22:39:40 -0400

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,15 @@
+language_input = input("What language you prefer English or Spanish?" )
+if language_input == "English":
+  print("Welcome,")
+  order_input =input("what do you want to order, pizza or pasta" )
+  if order_input == "pizza":
+    print("It would be $20")
+  if order_input == "pasta":
+      print("It would be $15")
+if language_input == "Spanish":
+  print("Bienvenido,")
+  orden_input =input("Que desea pedir, pizza o pasta")
+  if orden_input == "pizza":
+    print("Seria $20,000 bolivares")
+  if orden_input == "pasta":
+      print("Seria $15,000 bolivares")
